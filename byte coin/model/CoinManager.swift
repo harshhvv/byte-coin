1e0a171c38354a86d2255b297c361a0a8956185f
harshhvv <76173453+harshhvv@users.noreply.github.com>
2021-07-22 10:47:46 +0530

    .

---
+++ byte coin/model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  byte coin
+//
+//  Created by Harsh  on 22/07/21.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
